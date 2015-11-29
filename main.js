9f0d409ce08d8ef7efab154f87b70cc3d89e9982
comountainclimber <mlasky46@gmail.com>
2015-11-29 11:03:20 -0700

    ...

---
+++ main.js
@@ -0,0 +1,10 @@
+angular.module('App', []);
+
+angular.module('App')
+
+angular.module('App')
+	.controller('mainController', ['$scope', function($scope){
+
+		$scope.greeting = "hello world"
+
+		}])
